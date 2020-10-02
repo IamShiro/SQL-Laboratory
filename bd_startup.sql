cb68ecc2822572d29ace23e5cc88a15d62c18ebc
IamShiro <42015013+IamShiro@users.noreply.github.com>
2020-10-02 03:36:45 -0300

    Update bd_startup.sql

--- bd_startup.sql
+++ bd_startup.sql
@@ -4,7 +4,7 @@ use bd_startup;
 create table startup
 (
 	id_startup int primary key,
-    nome_startup varchar(50) unique
+    	nome_startup varchar(50) unique
 
 ) engine = InnoDB;
 
@@ -12,42 +12,43 @@ create table startup
 create table linguagem_programacao
 (
 	id_linguagem int primary key,
-    linguagem varchar(10) unique
+    	linguagem varchar(10) unique
 
 ) engine = InnoDB;
 
 create table programador 
 (
 	id_programador int primary key,
-    id_startup int,
-    nome_programador varchar(30),
+    	id_startup int,
+    	nome_programador varchar(30),
     
-    foreign key (id_startup) references startup (id_startup)
+    	foreign key (id_startup) references startup (id_startup)
     
 ) engine = InnoDB; -- para não bugar a foreign key
 
 create table programador_linguagem 
 (
 	id_programador int,
-    id_startup int,
+    	id_linguagem int,
 
 	foreign key (id_programador) references programador (id_programador),
-    foreign key (id_startup) references startup (id_startup)
+    	foreign key (id_linguagem) references linguagem_programacao (id_linguagem)
     
 ) engine = InnoDB;
 
-insert into startup (nome_startup) values 
+insert into startup values 
 (1, "Pitang"),
 (2, "Tempest"),
-(3, "Accenture");
+(3, "Accenture"),
+(4, "CESAR");
 
-insert into linguagem_programacao (linguagem) values 
+insert into linguagem_programacao values 
 (1, "C++"), -- 1
 (2, "Java"),
 (3, "Python"),
-(4,"Dart"),
+(4, "Dart"),
 (5, "C#"),
-(6, "Java Script");
+(6, "PHP");
 
 insert into programador values 
 (1, 2, "Pablo"), -- (id_programador, id_startup, nome_programador)
@@ -55,14 +56,15 @@ insert into programador values
 (3, 1, "Amanda"),
 (4, 1, "Danilo"),
 (5, 1, "João"),
-(6, 1, "Spicollys");
+(6, 1, "Spicollys"),
+(7, 1, "Lucas"),
+(8, 4, "Larissa");
 
 insert into programador_linguagem values 
 (1, 3), -- (id_programador, id_linguagem)
 (1, 2),
 (1, 4),
 (2, 5),
-(3, 3),
 (4, 3),
 (4, 2),
 (5, 3),
@@ -71,10 +73,3 @@ insert into programador_linguagem values
 (6, 2),
 (6, 4),
 (6, 6);
-
-
-
-
-
-
-
